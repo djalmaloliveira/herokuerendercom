f86e79b59048ed638eefaf1f22ee11db7500ebb7
Djalma Oliveira <djalmaloliveira@gmail.com>
2022-12-04 23:44:57 -0300

    Novas Tentativa

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.get('/')
 def index():
     return 'app renderizado'
 
 @app.get('/inscrito/<nome_inscrito>')
 def inscrito(nome_inscrito):
-    return f'Ola {nome_inscrito}'
-
+    return f"Olá {nome_inscrito}"
 
 if __name__ == '__main__':
     app.run(debug=True)
